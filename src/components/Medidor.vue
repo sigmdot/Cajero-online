4622827ab49f68f018145e669d32cb10d4e52c81
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-06-09 16:59:59 -0400

    add medidor componente, add Dashboard view

---
+++ src/components/Medidor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="medidor col container-fluid row pr-0 m-2">
+        <div class="col-8 border-right pr-0">
+            <div class="col-12 p-0">
+                <p class="text-info m-0 p-1">Venta semanal:</p>
+                <hr class="m-1">
+            </div>
+            <div class="col-12 p-0">
+                <h5>$ 9.999</h5>
+            </div>
+        </div>
+        <div class="col-4 w-100">
+            <b-icon icon="arrow90deg-up" variant="success"></b-icon>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Medidor',
+    components:{
+
+    }
+}
+</script>
+
+<style>
+   .medidor{
+       border: 1px solid black;
+       max-height: 88.3px;
+       overflow: hidden;
+   }
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dashboard">
+      <div class="container-fluid">
+        <div class="row justify-content-between">
+            <medidor></medidor>
+            <medidor></medidor>
+            <medidor></medidor>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Medidor from '@/components/Medidor.vue'
+export default {
+    name:'Dashboard',
+    components:{
+        Medidor
+    }
+}
+</script>
+
+<style>
+
+</style>
